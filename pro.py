586c61a1104085b406cd39d0419554da7d4ea87e
AdityaShane <adityasen_soumitra@srmuniv.edu.in>
2017-07-22 12:33:48 +0530

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,14 @@
+#Write a function that takes an exploder function and N. the exploder function explodes the given string into N times
+
+ #Function Prototype:
+    #Exploder(string,n)
+  #  Myfun(string,exploder,n)
+# Input: guvi 5
+ #Output: guviguviguviguviguvi 
+
+
+
+x = raw_input()
+def main1(yy,xx) :
+	print yy * int(xx)
+main1("guvi",x)
